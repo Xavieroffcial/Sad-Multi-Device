cd09becdfe6d03be9653aa7283fa822d5ddcf543
Santayngab <108774632+Santayngab@users.noreply.github.com>
2022-07-16 11:42:23 +0800

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*Private Ngab*\n\n Subscribe My Channel :https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg\n,Terimakasih`
+let str = `*Private Ngab*\n\n*Subscribe My Channel:*https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg\n\n*Terimakasih*`
 conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
 }
 handler.command = handler.help = ['sc']
