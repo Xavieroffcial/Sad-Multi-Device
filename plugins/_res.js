b6b954d6f90e983c1bc393a0061c8041c941a95a
Santayngab <108774632+Santayngab@users.noreply.github.com>
2022-07-17 21:29:38 +0800

    Update _res.js

--- plugins/_res.js
+++ plugins/_res.js
@@ -18,10 +18,10 @@ if (m.isBaileys) return
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.sendMessage(m.chat, { sticker : stc, thumbnail: await( await fetch(pp)).buffer() , contextInfo:{  externalAdReply: { showAdAttribution: true,
 mediaType:  1,
-mediaUrl: 'https://wa.me/17608914335',
+mediaUrl: 'https://wa.me/6285822347348',
 title: '「 ❔ 」',
 body: wm,
-sourceUrl: 'http://github.com/FahriAdison', thumbnail: await( await fetch(pp)).buffer()
+sourceUrl: 'http://github.com/Warikrr', thumbnail: await( await fetch(pp)).buffer()
  //Kalo mau ada pp dari sender/pengirimnya
  //Kalo mau ringan pake global.thumb                                                                                                              
                                                                                                                
