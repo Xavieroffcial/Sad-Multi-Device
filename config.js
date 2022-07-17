540d76bb9fe068c547d0e7ae2b7295fb5747b53e
Santayngab <108774632+Santayngab@users.noreply.github.com>
2022-07-17 18:01:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.owner = [
 global.mods = [] // Want some help?
 global.prems = ['6285822347348'] // Premium user has unlimited limit
 global.lolkey = 'Papah-Chan'
-global.xkey = 'BagasPrdn'
+global.xkey = ''
 global.APIs = { // API Prefix
   // name: 'https://website'
   lolhuman: 'https://api.lolhuman.xyz',
