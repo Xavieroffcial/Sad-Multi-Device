e87f940cb5775d429ecb4bc958c89d5f8883a2ef
Xavieroffcial <108806813+Xavieroffcial@users.noreply.github.com>
2022-08-20 23:48:36 +0800

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -4,8 +4,8 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Ingin Mendukung Bot?
 *PAYMENT ↓*
-_*Pulsa/Pulse(Indosat):*_ 085822347348
-_*Dana:*_ 085822347348
+_*Pulsa/Pulse(Axis):*_ 083862006229
+_*Dana:*_ 085962734007
 Setelah melakukan donasi kirim bukti pembayaran ke owner,Dengan melakukan donasi,kamu telah mensupport bot ini agar on 24 jam dan fast respon
 `
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg', 'My YouTube', null, null, [
